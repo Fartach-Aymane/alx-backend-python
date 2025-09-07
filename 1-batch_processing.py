6d0fbd2982da79d6baa279adbe77f373cbb8c242
Aymane Fartach <fartachayman@gmail.com>
2025-09-07 18:48:41 +0200

    Task 2: Batch processing generator fixed

--- 1-batch_processing.py
+++ 1-batch_processing.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
 import seed
 
 def stream_users_in_batches(batch_size):
-    """Generator to fetch users in batches"""
+    """
+    Generator to fetch users from database in batches.
+    Yields one batch at a time.
+    """
     offset = 0
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
@@ -18,7 +22,10 @@ def stream_users_in_batches(batch_size):
 
 
 def batch_processing(batch_size):
-    """Process each batch and print users older than 25"""
+    """
+    Process each batch and print users older than 25.
+    Uses the generator from stream_users_in_batches.
+    """
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
